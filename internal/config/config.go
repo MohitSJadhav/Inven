1fe056a9719355690744c882d90f18d0362b4c8f
Mohit Jadhav <mjadhav@tesla.com>
2024-04-24 12:51:32 -0500

    update commit

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,3 @@
 package config
 
-var (
-	DATABASE_USER      string = "avnadmin"
-	DATABASE_PASSWORD  string = "AVNS_hjiokHpX1flMJdkHFkZ"
-	DATABASE_NET       string = "tcp"
-	DATABASE_ADDRESS   string = "inventory-inventory-manager.b.aivencloud.com:11181"
-	DATABASE_NAME      string = "inventory"
-	URI_PARAM_HOSTNAME string = "inventory-inventory-manager.b.aivencloud.com"
-)
+var ()
